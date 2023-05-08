4dc2bedb1caadaf402f6dfb01e5c9fa5b216fe64
Darius <oldarius2002@gmail.com>
2023-05-08 10:59:37 +0300

    Implemented connection

--- Orders_Manager/src/main/java/connection/ConnectionFactory.java
+++ Orders_Manager/src/main/java/connection/ConnectionFactory.java
@@ -1,4 +1,65 @@
 package connection;
 
+import java.sql.*;
+
 public class ConnectionFactory {
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DBURL = "jdbc:mysql://localhost:3306/schooldb";
+    private static final String USER = "root";
+    private static final String PASS = "root";
+    private static ConnectionFactory singleInstance = new ConnectionFactory();
+
+    private ConnectionFactory(){
+        try{
+            Class.forName(DRIVER);
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    private Connection createConnection(){
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(DBURL, USER, PASS);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+
+    public static Connection getConnection(){
+        return singleInstance.createConnection();
+    }
+
+    public static void closeConnection(Connection connection){
+        if(connection != null){
+            try{
+                connection.close();
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void closeStatement(Statement statement){
+        if(statement != null){
+            try{
+                statement.close();
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet resultSet){
+        if(resultSet != null){
+            try{
+                resultSet.close();
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+    }
 }
